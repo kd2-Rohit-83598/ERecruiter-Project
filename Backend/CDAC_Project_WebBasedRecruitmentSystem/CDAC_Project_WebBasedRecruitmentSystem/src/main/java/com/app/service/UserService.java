8699db9c359efe838fcb8c8a8f32ef747aed6fbc
kd2-Rohit-83598 <rohitjadhav00700@gmail.com>
2024-12-09 22:02:45 +0530

    Updated The Project

---
+++ Backend/CDAC_Project_WebBasedRecruitmentSystem/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,30 @@
+package com.app.service;
+
+
+
+import com.app.payload.request.BasicDetailRequest;
+import com.app.payload.request.PersonalDetailRequest;
+import com.app.payload.request.ResetPassword;
+import com.app.payload.request.Signup;
+import com.app.payload.request.UserAndOtp;
+import com.app.payload.response.ApiResponse;
+import com.app.payload.response.OtpAndEmailResponse;
+import com.app.payload.response.UserDetailsResp;
+
+public interface UserService {
+	
+	//sign up
+	Signup userRegistration(Signup reqDTO);
+
+	UserDetailsResp getBasicDetail();
+	
+	ApiResponse updateBasicDetails(BasicDetailRequest basicDetails);
+
+	ApiResponse updatePersonalDetailFun(PersonalDetailRequest personalDetail);
+
+	ApiResponse forgotPassword(String email);
+
+	OtpAndEmailResponse verifyOtp(UserAndOtp userAndOtp);
+
+	ApiResponse resetPassword(String email,String otp,ResetPassword password);
+}
